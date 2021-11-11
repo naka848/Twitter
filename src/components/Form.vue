9c391275d6de04ea291ed5c01994ebf05c1fb0e0
naka848 <n.moeko1966@gmail.com>
2021-11-11 16:07:32 +0900

    変更

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -9,48 +9,80 @@
         cols="60"
         rows="5"
         placeholder="いまなにしとる？"
-        v-model="TweetObj.tweet_body"
+        v-model="data.TweetObj.tweet_body"
       >
       </textarea>
-      <br>
-      <button class="btn btn-info" 
-        v-on:click="doAction">ツイートする</button>
+      <br />
+      <button class="btn btn-info" v-on:click="doAction">ツイートする</button>
     </div>
   </div>
 </template>
 
 <script>
+import { onUpdated, reactive } from "vue";
+
 export default {
-  name: 'Form',
+  name: "Form",
   props: {
     title: String,
-    user:Object,
+    user: Object,
   },
-  data(){
-    return{
-      TweetObj:{
-        tweet_id:0,
-        tweet_body:'',
-        tweet_user:{
-          user_id:'id',
-          user_name:'test'
-        }
+
+  setup() {
+    const data = reactive({
+      TweetObj: {
+        tweet_id: 0,
+        tweet_body: "",
+        tweet_user: {
+          user_id: "id",
+          user_name: "test",
+        },
       },
-    }
+    });
+    console.log(data);
+
+    onUpdated(()=>{
+      console.log(data.TweetObj.tweet_user);
+    })
+    return {
+      data,
+    };
   },
-  methods:{
-    // ツイートするボタンがおされたら
-    // ①tweet_idを１増やす
-    // ②TweetObj（idと入力内容）をApp.vueに送り、result-eventが発火する
-    doAction(){ 
-      this.TweetObj.tweet_id += 1
-      this.TweetObj.tweet_user = this.user;
-      // （参照ではない）値をわたすために、新しいオブジェクトを定義する
-      // Object.assign(コピー先オブジェクト,コピー元オブジェクト)
-      const sendTweet = Object.assign({},this.TweetObj)
-      this.$emit('result-event', sendTweet)
+
+  // data(){
+  //   return{
+  //     TweetObj:{
+  //       tweet_id:0,
+  //       tweet_body:'',
+  //       tweet_user:{
+  //         user_id:'id',
+  //         user_name:'test'
+  //       }
+  //     },
+  //   }
+  // },
+
+  methods: {
+    doAction() {
+      this.data.TweetObj.tweet_id += 1;
+      this.data.TweetObj.tweet_user = this.data.user;
+      const sendTweet = Object.assign({}, this.data.TweetObj);
+      this.$emit("result-event", sendTweet);
     },
-    
-  }
-}
+  },
+
+  // methods:{
+  //   // ツイートするボタンがおされたら
+  //   // ①tweet_idを１増やす
+  //   // ②TweetObj（idと入力内容）をApp.vueに送り、result-eventが発火する
+  //   doAction(){
+  //     this.TweetObj.tweet_id += 1
+  //     this.TweetObj.tweet_user = this.user;
+  //     // （参照ではない）値をわたすために、新しいオブジェクトを定義する
+  //     // Object.assign(コピー先オブジェクト,コピー元オブジェクト)
+  //     const sendTweet = Object.assign({},this.TweetObj)
+  //     this.$emit('result-event', sendTweet)
+  //   },
+  // }
+};
 </script>
--- src/components/SelectUser.vue
+++ src/components/SelectUser.vue
@@ -7,36 +7,40 @@
       <option
         v-for="tweet_user in tweet_users"
         v-bind:value="tweet_user"
-        v-bind:key="tweet_user.user_id">
+        v-bind:key="tweet_user.user_id"
+      >
         {{ tweet_user.user_name }}
       </option>
     </select>
-    <br>
-    <button class="btn btn-info" v-on:click="changeUser">
-      変更する
-    </button>
+    <br />
+    <button class="btn btn-info" v-on:click="changeUser">変更する</button>
   </div>
 </template>
 
 <script>
 export default {
-    name: "SelectUser",
-    data(){
-        return{
-            tweet_users:[
-                { user_id: "test_a", user_name: "Aさん" },
-                { user_id: "test_b", user_name: "Bさん" },
-                { user_id: "test_c", user_name: "Cさん" },
-            ],
-            selectedUser: {
-                user_id: "test_a", user_name: "Aさん" 
-            },
-        };
+  name: "SelectUser",
+  data() {
+    return {
+      tweet_users: [
+        { user_id: "test_a", user_name: "Aさん" },
+        { user_id: "test_b", user_name: "Bさん" },
+        { user_id: "test_c", user_name: "Cさん" },
+      ],
+      selectedUser: {
+        user_id: "test_a",
+        user_name: "Aさん",
+      },
+    };
+  },
+
+  updated: function () {
+    console.log(this.data.TweetObj.tweet_user);
+  },
+  methods: {
+    changeUser() {
+      this.$emit("selectUser-event", this.selectedUser);
     },
-    methods: {
-        changeUser(){
-            this.$emit('selectUser-event',this.selectedUser)
-        }
-    },
+  },
 };
 </script>
